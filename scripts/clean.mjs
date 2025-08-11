2273412e503a5757572b2b8a433e8d94685e2b58
mr0be <bdt.xus72@gmail.com>
2025-08-11 14:14:30 +0200

    update config

--- scripts/clean.mjs
+++ scripts/clean.mjs
@@ -4,8 +4,3 @@ if (fs.existsSync('dist')) {
   fs.rmSync('dist', { recursive: true });
   console.log('✅ dist/ delete');
 }
-
-if (fs.existsSync('dist-cjs')) {
-  fs.rmSync('dist-cjs', { recursive: true });
-  console.log('✅ dist-cjs/ delete');
-}
